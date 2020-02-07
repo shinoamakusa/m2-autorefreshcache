5bd79146a4b857cd6bc85e1eeaf0fed307ea51ee
Oleg K <voknevark@gmail.com>
2020-02-06 18:17:44 -0800

    ObjectManager Inject

--- Helper/Data.php
+++ Helper/Data.php
@@ -1,29 +1,35 @@
 <?php
-
 namespace Hapex\AutoRefreshCache\Helper;
 
 use Hapex\Core\Helper\DataHelper;
+use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\App\Helper\Context;
 
 class Data extends DataHelper
 {
-    public function isEnabled()
-    {
-        return $this->getConfigFlag('hapex_autorefreshcache/general/enable');
-    }
-    
-    public function isCacheCleanEnabled()
-    {
-        return $this->getConfigFlag('hapex_autorefreshcache/options/clean_enable');
-    }
-    
-    public function isCacheFlushEnabled()
-    {
-        return $this->getConfigFlag('hapex_autorefreshcache/options/flush_enable');
-    }
-    
-    public function log($message)
-    {
-        $this->printLog("hapex_cache_refresh", $message);
-    }
+	public function __construct(Context $context, ObjectManagerInterface $objectManager)
+	{
+
+		parent::__construct($context, $objectManager);
+	}
+
+	public function isEnabled()
+	{
+		return $this->getConfigFlag('hapex_autorefreshcache/general/enable');
+	}
+
+	public function isCacheCleanEnabled()
+	{
+		return $this->getConfigFlag('hapex_autorefreshcache/options/clean_enable');
+	}
+
+	public function isCacheFlushEnabled()
+	{
+		return $this->getConfigFlag('hapex_autorefreshcache/options/flush_enable');
+	}
+
+	public function log($message)
+	{
+		$this->printLog("hapex_cache_refresh", $message);
+	}
 }
