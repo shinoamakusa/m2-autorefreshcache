060afcf31b2cc31ca0f9367a7c5a3f70a71dbc3a
Oleg K <voknevark@gmail.com>
2020-07-20 09:27:09 -0700

    Update Cron/CacheRefresh.php

--- app/code/Hapex/AutoRefreshCache/Cron/CacheRefresh.php
+++ app/code/Hapex/AutoRefreshCache/Cron/CacheRefresh.php
@@ -25,49 +25,9 @@ class CacheRefresh
             case true:
                 $this->helperData->log("");
                 $this->helperData->log("Starting Auto Cache Refresh");
-                try {
-                    //$cache_types = array('config','layout','block_html','collections','reflection','db_ddl','eav','config_integration','config_integration_api','full_page','translate','config_webservice');
-                    $this->helperData->log("- Getting cache types list");
-                    $cache_types = array_keys($this->_cacheTypeList->getTypes());
-                    $total = count($cache_types);
-                    $this->helperData->log("- Found $total cache types");
-
-                    switch ($this->helperData->isCacheCleanEnabled()) {
-                        case true:
-                            $this->helperData->log("- Cleaning the cache");
-                            foreach ($cache_types as $type) {
-                                $this->_cacheTypeList->cleanType($type);
-                                $this->helperData->log("-- Cleaned cache type '$type'");
-                            }
-                            $this->helperData->log("- Cleaned the cache");
-                            break;
-
-                        default:
-                            $this->helperData->log("- Cache cleaning is disabled");
-                            break;
-                    }
-
-                    switch ($this->helperData->isCacheFlushEnabled()) {
-                        case true:
-                            $this->helperData->log("- Flushing the cache");
-                            $count = 0;
-                            foreach ($this->_cacheFrontendPool as $cache_clean_flush) {
-                                $cache_clean_flush->getBackend()->clean();
-                                $count++;
-                            }
-                            $count = $count != 0 ? $count : "No";
-                            $this->helperData->log("- Flushed $count cache types");
-                            break;
-
-                        default:
-                            $this->helperData->log("- Cache flushing is disabled");
-                            break;
-                    }
-                } catch (\Exception $e) {
-                    $this->helperData->log("Error: " . $e->getMessage());
-                } finally {
-                    $this->helperData->log("Ending Auto Cache Refresh");
-                }
+                $this->doCacheClean();
+                $this->doCacheFlush();
+                $this->helperData->log("Ending Auto Cache Refresh");
                 break;
 
             default:
@@ -75,5 +35,57 @@ class CacheRefresh
                 break;
         }
         return $this;
+    }
+
+    private function doCacheClean()
+    {
+        try {
+            //$cache_types = array('config','layout','block_html','collections','reflection','db_ddl','eav','config_integration','config_integration_api','full_page','translate','config_webservice');
+            $this->helperData->log("- Getting cache types list");
+            $cache_types = array_keys($this->_cacheTypeList->getTypes());
+            $total = count($cache_types);
+            $this->helperData->log("- Found $total cache types");
+
+            switch ($this->helperData->isCacheCleanEnabled()) {
+              case true:
+              $this->helperData->log("- Cleaning the cache");
+              foreach ($cache_types as $type) {
+                  $this->_cacheTypeList->cleanType($type);
+                  $this->helperData->log("-- Cleaned cache type '$type'");
+              }
+              $this->helperData->log("- Cleaned the cache");
+              break;
+
+              default:
+              $this->helperData->log("- Cache cleaning is disabled");
+              break;
+            }
+        } catch (\Exception $e) {
+            $this->helperData->errorLog(__METHOD__, $e->getMessage());
+        }
+    }
+
+    private function doCacheFlush()
+    {
+        try {
+            switch ($this->helperData->isCacheFlushEnabled()) {
+              case true:
+              $this->helperData->log("- Flushing the cache");
+              $count = 0;
+              foreach ($this->_cacheFrontendPool as $cache_clean_flush) {
+                  $cache_clean_flush->getBackend()->clean();
+                  $count++;
+              }
+              $count = $count != 0 ? $count : "No";
+              $this->helperData->log("- Flushed $count cache types");
+              break;
+
+              default:
+              $this->helperData->log("- Cache flushing is disabled");
+              break;
+      }
+        } catch (\Exception $e) {
+            $this->helperData->errorLog(__METHOD__, $e->getMessage());
+        }
     }
 }
