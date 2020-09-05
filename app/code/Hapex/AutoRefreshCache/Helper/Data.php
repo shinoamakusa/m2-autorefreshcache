2e4a43fc68bd182fc9210a5f89d7323b1ce872f0
Oleg K <voknevark@gmail.com>
2020-09-05 09:17:21 -0700

    Update Helper\Data.php

--- app/code/Hapex/AutoRefreshCache/Helper/Data.php
+++ app/code/Hapex/AutoRefreshCache/Helper/Data.php
@@ -3,8 +3,6 @@
 namespace Hapex\AutoRefreshCache\Helper;
 
 use Hapex\Core\Helper\DataHelper;
-use Magento\Framework\App\Helper\Context;
-use Magento\Framework\ObjectManagerInterface;
 
 class Data extends DataHelper
 {
@@ -12,12 +10,6 @@ class Data extends DataHelper
     protected const XML_PATH_CONFIG_ENABLED_CLEAN = "hapex_autorefreshcache/options/clean_enable";
     protected const XML_PATH_CONFIG_ENABLED_FLUSH = "hapex_autorefreshcache/options/flush_enable";
     protected const FILE_PATH_LOG = "hapex_cache_refresh";
-
-    public function __construct(Context $context, ObjectManagerInterface $objectManager)
-    {
-
-        parent::__construct($context, $objectManager);
-    }
 
     public function isEnabled()
     {
